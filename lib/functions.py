f06620ee237914435142876e358224e65fda046c
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-09-10 20:56:00 -0700

    lab completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-
 def greet_programmer():
-    pass
+    print("Hello, programmer", end="!\n")
 
-def greet(name):
-    pass
+def greet(name="Naureen"):
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)): 
+        return None
+    else:
+        return number/2
